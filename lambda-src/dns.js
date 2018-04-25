94dbb1af9c22e878bd3c04d39c3f14653758aab3
Harry Green <harry@hgreen.co.uk>
2018-04-26 00:12:47 +0100

    Add lambda function

---
+++ lambda-src/dns.js
@@ -0,0 +1,10 @@
+const dns = require('dns');
+
+exports.handler = function (event, context, callback) {
+  dns.resolve(event.queryStringParameters.hostname, 'ANY', (err, records) => {
+    callback(err, {
+      statusCode: 200,
+      body: JSON.stringify(records)
+    });
+  })
+}
